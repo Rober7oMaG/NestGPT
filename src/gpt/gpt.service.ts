26937b8c7d8eab705e05a2d9b54c5a256b71e359
Roberto Macias <robertomag7@hotmail.com>
2024-01-28 19:17:05 -0700

    feature: orthography use case implementation

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 import { orthographyCheckUseCase } from './use-cases';
+import { OrthographyDTO } from './dtos';
 
 @Injectable()
 export class GptService {
-  async orthographyCheck() {
-    return await orthographyCheckUseCase();
+  private openAI = new OpenAI({
+    apiKey: process.env.OPEN_AI_API_KEY,
+  });
+
+  async orthographyCheck(orthographyDTO: OrthographyDTO) {
+    return await orthographyCheckUseCase(this.openAI, {
+      prompt: orthographyDTO.prompt,
+    });
   }
 }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,5 +1,45 @@
-export const orthographyCheckUseCase = async () => {
-  return {
-    hello: 'World',
-  };
+import OpenAI from 'openai';
+
+type Options = {
+  prompt: string;
+};
+
+export const orthographyCheckUseCase = async (
+  openAI: OpenAI,
+  options: Options,
+) => {
+  const { prompt } = options;
+
+  const completion = await openAI.chat.completions.create({
+    messages: [
+      {
+        role: 'system',
+        content: `
+          You will be provided with texts in English or Spanish that may contain grammatical and orthographic errors. 
+          
+          Your task is to correct the errors and make the text grammatically and orthographically correct, and also to give a percentage of the text's correctness. If there are no errors in the text, you should return a congratulations message.
+
+          Your response must be in JSON format.
+          Here is an example of how to structure your response:
+          {
+            "userScore": number,
+            "errors": string[], // ['error -> correction', ...]
+            "message": string,
+          }
+        `,
+      },
+      { role: 'user', content: prompt },
+    ],
+    model: 'gpt-3.5-turbo',
+    temperature: 0.3,
+    max_tokens: 150,
+    //* This may be invalid in certain models
+    response_format: {
+      type: 'json_object',
+    },
+  });
+
+  const jsonResponse = JSON.parse(completion.choices[0].message.content);
+
+  return jsonResponse;
 };
